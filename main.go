620cc291be6694b8689ec978a274a2f38b8b6fbc
coolabhays <abhaysp9955@gmail.com>
2022-03-12 19:44:46 +0530

    adding new pages with if/else

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if req.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my site!</h1>")
+	} else if req.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please send an email to " +
+			"<a href=\"mailto:support@lenslocked.com\">" +
+			"support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		// if WriteHeader is not called before Write(), the first call to Write
+		// will trigger implicit WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "<h1>We couldn't find the page you are looking for :(</h1>" +
+			"<p>Please email us if you keep being sent to an invalid page</p>")
+	}
 }
 
 func main() {

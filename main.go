0266312be38d32be98e8d536fc0d822ffd8fd0be
coolabhays <abhaysp9955@gmail.com>
2022-03-12 21:12:16 +0530

    using gorilla/mux for page routing

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if req.URL.Path == "/" {
-		fmt.Fprintf(w, "<h1>Welcome to my site!</h1>")
-	} else if req.URL.Path == "/contact" {
-		fmt.Fprintf(w, "To get in touch, please send an email to " +
-			"<a href=\"mailto:support@lenslocked.com\">" +
-			"support@lenslocked.com</a>.")
+/* func handlerFunc(w http.ResponseWriter, req *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		// if WriteHeader is not called before Write(), the first call to Write
@@ -20,11 +15,26 @@ func handlerFunc(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "<h1>We couldn't find the page you are looking for :(</h1>" +
 			"<p>Please email us if you keep being sent to an invalid page</p>")
 	}
+} */
+
+func home(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Welcome to my site!</h1>")
+}
+
+func contact(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "To get in touch, please send an email to " +
+		"<a href=\"mailto:support@lenslocked.com\">" +
+		"support@lenslocked.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()  // like we do with http.NewServeMux()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq_helper)
+	http.ListenAndServe(":3000", r)
 }
 
 
